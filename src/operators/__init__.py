771e9032f56b104d0fcab35f12b53eea19b5201a
Tarabon4ik <68551045+Tarabon4ik@users.noreply.github.com>
2023-12-13 11:26:58 +0200

    Delete src directory

--- src/operators/__init__.py
+++
@@ -1,6 +0,0 @@
-from .core import ComparisonOperators, MembershipOperators, LogicalOperators, Operators
-
-comparison_operators = ComparisonOperators()
-membership_operators = MembershipOperators()
-logical_operators = LogicalOperators()
-operators = Operators()
